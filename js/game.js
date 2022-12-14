f2a28d5e4e4df1f0a6774f17762a93e9583929ba
Larissa Barbosa <larissabarbosa@id.uff.br>
2022-12-14 20:52:12 -0300

    done game

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'familia-adams01',
@@ -26,7 +28,8 @@ const checkEndGame = () => {
     const disableCards = document.querySelectorAll('.disable-card');
 
     if (disableCards.length === 20) {
-        alert('Parabéns! Você conseguiu!');
+        clearInterval(this.loop);
+        alert(`Parabéns,${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML} segundos!`);
     }
 }
 
@@ -110,4 +113,19 @@ const loadGame = () => {
     });
 }
 
-loadGame();
+const startTimer = () => {
+
+    this.loop = setInterval(() => {
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime +1;
+    }, 1000);
+
+}
+
+window.onload = () => {
+    spanPlayer.innerHTML = localStorage.getItem('player');
+
+    startTimer();
+    loadGame();
+}
+
